b2b0932357904cec1d2fdad3b76cb817b45412d5
yangzhichao01 <camike_huihui@sina.com>
2021-08-24 08:55:28 +0800

    server demo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 var http = require('http');
+var url = require('url');
+var queryString = require('queryString');
 
 http.createServer(function (request, response) {
+    if (request.method == "POST") {
+        var postData = ''
+        request.on('data', (chunk)=>{
+            postData += chunk;
+        })
+        request.on('end', ()=>{
+            postData = queryString.parse(postData);
+            console.log(postData);
+        })
+    }
+    
 
+    var clientUrl = new URL(request.url, 'http://localhost/');
+    clientUrl.searchParams.forEach((value, key) => {
+        console.log(key+'='+value);
+    })
+ 
     // 发送 HTTP 头部 
     // HTTP 状态值: 200 : OK
     // 内容类型: text/plain
@@ -12,4 +30,4 @@ http.createServer(function (request, response) {
 }).listen(3001);
 
 // 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://127.0.0.1:3001/');
